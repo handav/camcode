c9aad78d35af9894c8bbd72bf3d29ecd74cdc1e5
handav <hannahdavis0220@gmail.com>
2018-08-07 20:59:52 -0400

    updating text

--- text/text.py
+++ text/text.py
@@ -3,16 +3,10 @@ print 'hello world'
 harry_potter = open('harryPotter.txt', 'r')
 #print harry_potter.read()
 hp = harry_potter.read()
-hp = hp.decode('utf-8')
+#hp = hp.decode('utf-8')
 print hp
 
 import nltk
-# words = nltk.wordpunct_tokenize(hp)
-# print words
-
-# for word in words:
-#     if word[0].isupper():
-#         print word
 
 sentences = nltk.sent_tokenize(hp)
 for s in sentences:
@@ -20,5 +14,3 @@ for s in sentences:
     for w in words:
         if w[0].isupper():
             print w
-
-
